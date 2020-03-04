c0d32e9552cc9aaddc0d94bcc747d05cd12a6583
Murat Saglam <42717642+Murat-U-Saglam@users.noreply.github.com>
2020-03-04 15:52:30 +0000

    Add files via upload

---
+++ CreatingNetworkPackets.py
@@ -0,0 +1,80 @@
+import re
+
+def srcIP():
+    ipAddress = input("What is the source IP Address: ")
+    validIpRegex = re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$",ipAddress)
+    while validIpRegex != None:
+        ipAddress = input("Please enter a valid IP Address: ")
+    return (ipAddress)
+
+def dstIP():
+    ipAddress = input("What is the destination IP Address: ")
+    validIpRegex = re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$",ipAddress)
+    while validIpRegex != None:
+        ipAddress = input("Please enter a valid IP Address: ")
+    return (ipAddress)
+
+def srcMAC():
+    macAddress = input("What is the source MAC Address: ")
+    validMacAddress = re.search(r'([0-9A-F]{2}[:-]){5}([0-9A-F]{2})', macAddress, re.I).group()
+    while validMacAddress != None:
+        macAddress = input("Please enter a valid MAC address: ")
+    return (macAddress)
+
+def dstMAC():
+    macAddress = input("What is the source MAC Address: ")
+    validMacAddress = re.search(r'([0-9A-F]{2}[:-]){5}([0-9A-F]{2})', macAddress, re.I).group()
+    while validMacAddress != None:
+        macAddress = input("Please enter a valid MAC address: ")
+    return (macAddress)
+
+def UDPCreate():
+    print("UDP File format")
+
+def TCPCreate():
+    print("TCP File format")
+
+def ICMPCreate():
+    print("ICMP file format")
+
+def HTTPCreate():
+    print("HTTP File format")
+
+def HTTPSCreate():
+    print("HTTPS file format")
+
+def ARPCreate():
+    print("ARP file format")
+
+def DHCPCreate():
+    print("DHCP file format")
+
+#Function used to define the User's desired protocol
+def NetworkPacketCreate():
+    while True:
+        protocol = input("What protocol PCAP file do you want to create?\nUDP\tTCP\tICMP\tHTTP\tHTTPS\tARP\tDHCP\n")
+        if protocol == "UDP" or protocol == "udp":
+            UDPCreate()
+            break
+        elif protocol == "TCP" or protocol == "tcp":
+            TCPCreate()
+            break
+        elif protocol == "ICMP" or protocol == "icmp":
+            ICMPCreate()
+            break
+        elif protocol == "HTTP" or protocol == "http":
+            HTTPCreate()
+            break
+        elif protocol == "HTTPS" or protocol == "https":
+            HTTPSCreate()
+            break
+        elif protocol == "ARP" or protocol == "arp":
+            ARPCreate()
+            break
+        elif protocol == "DHCP" or protocol == "dhcp":
+            DHCPCreate()
+            break
+        else:
+            print("Invalid protocol, please use one of the protocols defined")
+
+NetworkPacketCreate()
