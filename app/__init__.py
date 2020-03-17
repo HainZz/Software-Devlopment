5d91ee15d8460e8f9aecda6ee924b5fc2d566f06
HainZz <jack.hainsworth3@gmail.com>
2020-03-17 19:20:03 +0000

    Implementation of a flasks loginmanager and database and database migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from config import Config
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 app = Flask(__name__)
+app.config.from_object(Config)
 bootstrap = Bootstrap(app)
-from app import routes
+login = LoginManager(app)
+login.login_view = 'login'
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+from app import routes, models
