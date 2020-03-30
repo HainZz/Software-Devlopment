bc71b68f5e952a12572286af546a9648a2885ad6
DaPoliceman <61709812+DaPoliceman@users.noreply.github.com>
2020-03-30 15:01:22 +0100

    Add files via upload

--- pcapreader.c
+++ pcapreader.c
@@ -1,24 +1,25 @@
-/* https://elf11.github.io/2017/01/22/libpcap-in-C.html */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+/* defines the IP protcols, UDP, TCP, ICMP */
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <net/if.h>
 #include <netinet/if_ether.h>
+/* used to determine if the packet is ip, arp, etc. */
 #include <net/ethernet.h>
 #include <netinet/tcp.h>
 #include <netinet/udp.h>
 #include <arpa/inet.h>
-
 
 #include <pcap.h>
 
 #define PCAP_BUF_SIZE	100000
 #define PCAP_SRC_FILE	2
 
-void packetprocess();
+void packetprocess(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_char* packet);
 
 int main(int argc, char **argv){
 	char errbuf[PCAP_ERRBUF_SIZE];
@@ -45,7 +46,66 @@ int main(int argc, char **argv){
 }
 
 
-void packetprocess(){ /* where the packets are poked around with, some crazy shits happening here */
+void packetprocess(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_char* packet){ /* where the packets are poked around with, some crazy shits happening here */
+	const struct ether_header* ethernetHeader; /* used to determine wether the packet is an IP packet or not */
+	/* check for TCP, UDP, ICMP, IP, IPV6 */
+	const struct ip* ipHeader;
+	char sourceIP[INET_ADDRSTRLEN];
+   	char destIP[INET_ADDRSTRLEN];
 
+	/* TCP, UDP, ICMP */
+   	ethernetHeader = (struct ether_header*)packet;
+	if (ntohs(ethernetHeader->ether_type) == ETHERTYPE_IP){
+		printf("%s", "  IP  ,");
+		/* fill out the ip header, gets the info from ether header? unwraps it? */
+		ipHeader = (struct ip*)(packet + sizeof(struct ether_header));
+		/* inet_ntop - convert IPv4 and IPv6 addresses from binary to text form*/
+		/* we save the source and destination ip's into variables here */
+		inet_ntop(AF_INET, &(ipHeader->ip_src), sourceIP, INET_ADDRSTRLEN);
+        	inet_ntop(AF_INET, &(ipHeader->ip_dst), destIP, INET_ADDRSTRLEN);
+
+		/* is the header TCP? */
+		if (ipHeader -> ip_p == IPPROTO_TCP){
+			printf("%s", "  TCP  ,");
+			
+		
+		
+		/* fuck it, lets print the ip's too */
+		printf("  src: %15s    dst: %15s ,", sourceIP, destIP);
+		}
+		
+		/* is the header UDP? */
+		if (ipHeader -> ip_p == IPPROTO_UDP){
+			printf("%s", "  UDP  ,");
+			
+		
+		
+		/* fuck it, lets print the ip's too */
+		printf("  src: %15s    dst: %15s ,", sourceIP, destIP);
+		}
+	}	
+	if (ntohs(ethernetHeader->ether_type) == ETHERTYPE_ARP){
+		printf("%s", "APR");
+	}
+	if (ntohs(ethernetHeader->ether_type) == ETHERTYPE_REVARP){
+		printf("%s", "REVARP");
+	}
+	if (ntohs(ethernetHeader->ether_type) == ETHERTYPE_IPV6){
+		printf("%s", "IPv6");
+	}
+	if (ntohs(ethernetHeader->ether_type) == ETHERTYPE_LOOPBACK){
+		printf("%s", "LOOPbck");
+	}
+ 
+
+	/* \n for line break, dont include it anywhere else */
+	printf("\n");
 
 }
+
+
+
+
+
+
+
