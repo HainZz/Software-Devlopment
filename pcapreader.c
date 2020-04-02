7070522364e93a834802e8d583fa84b2c76eab48
DaPoliceman <61709812+DaPoliceman@users.noreply.github.com>
2020-04-02 14:24:25 +0100

    Added functionality for source address count
    the source address of each packet is recorded and after all packets are analyzed a tally is given

--- pcapreader.c
+++ pcapreader.c
@@ -19,6 +19,13 @@
 #define PCAP_BUF_SIZE	100000
 #define PCAP_SRC_FILE	2
 
+/* which synidx has the most syn counts */
+int pctCount[PCAP_BUF_SIZE];
+/* how many syns have been sent, used to iterate through  */
+int pctIdx = 0;
+/* stores the ipnumber of the packet */
+char pctIP[PCAP_BUF_SIZE][INET_ADDRSTRLEN];
+
 void packetprocess(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_char* packet);
 
 int main(int argc, char **argv){
@@ -42,6 +49,13 @@ int main(int argc, char **argv){
 		return 0;
 	}
 
+
+	for (int i = 0; i < pctIdx; i++) {
+		printf("ip: %18s, count: %d \n", pctIP[i], pctCount[i]);
+	}
+        
+
+	
 	return 0;
 }
 
@@ -52,7 +66,11 @@ void packetprocess(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_c
 	const struct ip* ipHeader;
 	char sourceIP[INET_ADDRSTRLEN];
    	char destIP[INET_ADDRSTRLEN];
+	int loopcheck = 0;
+   	
+	
 
+	
 	/* TCP, UDP, ICMP */
    	ethernetHeader = (struct ether_header*)packet;
 	if (ntohs(ethernetHeader->ether_type) == ETHERTYPE_IP){
@@ -62,7 +80,7 @@ void packetprocess(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_c
 		/* inet_ntop - convert IPv4 and IPv6 addresses from binary to text form*/
 		/* we save the source and destination ip's into variables here */
 		inet_ntop(AF_INET, &(ipHeader->ip_src), sourceIP, INET_ADDRSTRLEN);
-        	inet_ntop(AF_INET, &(ipHeader->ip_dst), destIP, INET_ADDRSTRLEN);
+        inet_ntop(AF_INET, &(ipHeader->ip_dst), destIP, INET_ADDRSTRLEN);
 
 		/* is the header TCP? */
 		if (ipHeader -> ip_p == IPPROTO_TCP){
@@ -85,7 +103,7 @@ void packetprocess(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_c
 		}
 	}	
 	if (ntohs(ethernetHeader->ether_type) == ETHERTYPE_ARP){
-		printf("%s", "APR");
+		printf("%s", "ARP");
 	}
 	if (ntohs(ethernetHeader->ether_type) == ETHERTYPE_REVARP){
 		printf("%s", "REVARP");
@@ -96,12 +114,38 @@ void packetprocess(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_c
 	if (ntohs(ethernetHeader->ether_type) == ETHERTYPE_LOOPBACK){
 		printf("%s", "LOOPbck");
 	}
- 
+	if (1==1) {
+            for (int i = 0; i < pctIdx; i++) {
+                if (strcmp(sourceIP, pctIP[i]) == 0) {
+	            	pctCount[i] = pctCount[i] + 1;
+					loopcheck = 1;
+					printf("why");
+                }
+            }
+	} 
+	if (loopcheck == 0){
+		printf("what");
+		strcpy(pctIP[pctIdx], sourceIP);
+        pctCount[pctIdx] = 1;
+		pctIdx = pctIdx + 1;
+		
+	}
+	loopcheck = 0;
+	            
 
 	/* \n for line break, dont include it anywhere else */
 	printf("\n");
 
 }
+
+
+
+
+
+
+
+
+
 
 
 
